20ddcbb8276d214a59965f45513be144491866c2
jkl-rgb <jkl@art-form.com>
2026-05-05 15:37:49 +0000

    Add CSV template download feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,49 @@ def zip_dir(folder):
 
 st.title("Screenwall Makr")
 
+template_headers = [
+    "panel_id",
+    "width",
+    "height",
+    "thickness",
+    "flange_type",
+    "flange1_depth",
+    "flange2_depth",
+    "hole_diameter",
+    "hole_pitch",
+    "pattern",
+    "fastening_pair",
+]
+
+template_rows = [
+    [
+        "example_panel",
+        "12.0",
+        "24.0",
+        "0.0800",
+        "plain",
+        "1.0",
+        "1.0",
+        "0.125",
+        "1.0",
+        "round",
+        "none",
+    ]
+]
+
+template_csv = ",".join(template_headers) + "\n"
+for row in template_rows:
+    template_csv += ",".join(row) + "\n"
+
+st.download_button(
+    "Download CSV input template",
+    data=template_csv,
+    file_name="screenwall_template.csv",
+    mime="text/csv",
+)
+
+st.caption("Use the downloaded CSV template to populate your panel data, then upload it below.")
+
 csv = st.file_uploader("Upload CSV")
 
 if csv:
